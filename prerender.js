0c755f51bd24a8474f21b7e4b093a3b2fad3b2f2
Roi Kingon <rkingon@gmail.com>
2020-12-09 08:38:52 -0800

    fix bugs

--- prerender.js
+++ prerender.js
@@ -5,7 +5,7 @@ function shouldPrerender ({ userAgent, uri }) {
   if(path.extname(uri)) {
     return false
   }
-  if(userAgent.includes('headless')) {
+  if(userAgent.includes('lighthouse')) {
     return true
   }
   if(userAgent.includes('mozilla')) {
@@ -17,11 +17,12 @@ function shouldPrerender ({ userAgent, uri }) {
 const cache = {}
 
 module.exports = async(req, res, next) => {
-  const url = `${req.protocol}://${req.get('host')}${req.originalUrl}`
+  const uri = req.originalUrl
+  const url = `${req.protocol}://${req.get('host')}${uri}`
   const userAgent = (req.headers['user-agent'] || '').toLowerCase()
-  if(shouldPrerender({ userAgent, uri: req.originalUrl })) {
+  if(shouldPrerender({ userAgent, uri })) {
     try {
-      if(!cache[url]) {
+      if(!cache[uri]) {
         const browser = await puppeteer.launch({
           args: [
             '--no-sandbox',
@@ -31,10 +32,10 @@ module.exports = async(req, res, next) => {
         const page = await browser.newPage()
         await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36')
         await page.goto(url, { waitUntil: 'networkidle0' })
-        cache[url] = await page.content()
+        cache[uri] = await page.content()
         await browser.close()
-        return res.send(cache[url])
       }
+      return res.send(cache[uri])
     } catch (err) {
       next()
     }
